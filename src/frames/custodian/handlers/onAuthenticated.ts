6b07333cd869fe73640d9996fed65df4df5c09a7
Iharob Al Asimi <iharob@gmail.com>
2021-01-07 20:45:10 -0400

    Removes the mnemonic modal flow

--- src/frames/custodian/handlers/onAuthenticated.ts
+++ src/frames/custodian/handlers/onAuthenticated.ts
@@ -1,7 +1,6 @@
 import { Message } from "../../../types/message";
 import { createMnemonic } from "../../signer/helpers/createMnemonic";
 import { GDriveApi } from "../gDriveApi";
-import { showSaveModal } from "../helpers/showSaveModal";
 import NotFoundError = GDriveApi.NotFoundError;
 
 export const onAuthenticated = async (): Promise<Message | null> => {
@@ -14,15 +13,10 @@ export const onAuthenticated = async (): Promise<Message | null> => {
     };
   } catch (error: any) {
     if (error.message === NotFoundError.message) {
-      const mnemonic: string = await createMnemonic();
-      // If this throws we are not catching the error so it
-      // will be propagated to the caller
-      await showSaveModal(mnemonic);
-      // If nothing was thrown, then good
       return {
         target: "Signer",
         type: "Initialize",
-        data: mnemonic,
+        data: await createMnemonic(),
       };
     } else {
       // Since this is not an special error, please rethrow it
